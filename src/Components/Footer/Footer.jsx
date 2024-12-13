11346cbf85db390dbea56eaf728c743668c86bd3
srijanibanerje <srijaniofficial2022@gmail.com>
2024-12-13 12:33:56 +0530

    "done"

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -26,6 +26,12 @@ const Footer = () => {
                   </div>
                 </li>
                 <li className="media">
+                  <div className="pull-left"> <span className="icon fa-stack fa-sm"> <i className="fa fa-map-marker fa-stack-1x fa-inverse"></i> </span> </div>
+                  <div className="media-body" style={{marginLeft:"15%"}}>
+                    <p>Corporate office address: Shakshi Appartment, Ground floor 6 no Road, HB Town, Sodepur, Kol-700110</p>
+                  </div>
+                </li>
+                <li className="media">
                   <div className="pull-left"> <span className="icon fa-stack fa-sm"> <i className="fa fa-mobile fa-stack-1x fa-inverse"></i> </span> </div>
                   <div className="media-body"  style={{marginLeft:"15%"}}>
                     <p> + (91)033-3591-8727</p><p>  +(91)7980964516 </p>
