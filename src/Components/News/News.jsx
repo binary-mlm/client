c7b5b26b92db1940a9a3527a82a79b7e208b4b17
srijanibanerje <srijaniofficial2022@gmail.com>
2025-02-20 15:48:59 +0530

    "donenewsupdate"

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import news from "../../assets/images/news.png";
 import new1 from "../../assets/images/News/udbhab_news.jpg";
+import new2 from "../../assets/images/News/mondarmonitour.jpg";
 import "./news.css";
 
 const News = () => {
   return (
    <>
     <img src={news} style={{width:"100%"}}/>
-    <div className='container w-75'>
-    <div className='d-flex justify-content-center my-4'>
-      <img className='newsimg' src={new1} style={{width:"80%" }}/>
-    </div> 
-    </div>
+    <div className="container w-75">
+        <div className="d-flex flex-column align-items-center my-4">
+          <div className="d-flex justify-content-center w-100">
+            <img className="newsimg" src={new2} style={{ width: "80%" }} alt="News 2" />
+          </div>
+          <div className="d-flex justify-content-center w-100 mt-3">
+            <img className="newsimg" src={new1} style={{ width: "80%" }} alt="News 1" />
+          </div>
+        </div>
+      </div>
    </>
   )
 }
